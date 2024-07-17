8436c7dfd00741fe4fffba55e736639a49e9851a
Tedros Kahsu <tedkahcom@gmail.com>
2024-07-17 14:59:50 +0300

    Working with lists

---
+++ Chapter 04 Working With Lists/magicians.py
@@ -0,0 +1,23 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    # print(magician)
+    print(f"{magician.title()}, that was a great trick")
+
+
+
+habesha_foods = ['ingera', 'dorowet', 'shiro']
+for food in habesha_foods:
+    print(food)
+
+
+#range()
+for value in range(1,5):
+    print(value)
+
+#Using range() to make a list of numbers
+numbers = list(range(1,6))
+print(numbers)
+
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
---
+++ Chapter 04 Working With Lists/players.py
@@ -0,0 +1,5 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[-3:])
+
+#copying a list
+new_players = players[:]                     
